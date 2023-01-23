01f81a086daaefbac4f52ad3b758a6e68c022700
BALLOF Thibault <thibault.ballof@worldline.com>
2023-01-23 16:52:43 +0100

    Adding Movies Model

---
+++ training/Model/Movies.swift
@@ -0,0 +1,28 @@
+//
+//  Movies.swift
+//  training
+//
+//  Created by Thibault Ballof on 23/01/2023.
+//
+
+import Foundation
+
+struct Movies: Decodable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Decodable {
+    let backdropPath: String
+    let title: String
+    let posterPath: String
+    let releaseDate: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case backdropPath = "backdrop_path"
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+       
+    }
+}
