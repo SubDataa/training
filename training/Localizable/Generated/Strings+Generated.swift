b870a850b49094da539afb0b258fa1c61d7f7e9e
Thibault Ballof <74681414+SubDataa@users.noreply.github.com>
2023-02-07 11:34:49 +0100

    Adding SwiftGen

---
+++ training/Localizable/Generated/Strings+Generated.swift
@@ -0,0 +1,43 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// Mock
+  internal static let mock = L10n.tr("Localizable", "Mock", fallback: "Mock")
+  /// Localizable.strings
+  ///   training
+  /// 
+  ///   Created by Thibault Ballof on 06/02/2023.
+  internal static let navTitle = L10n.tr("Localizable", "NavTitle", fallback: "POPULAR MOVIES")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
